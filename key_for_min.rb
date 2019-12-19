3c0c51ed4002f44a92f7b27910e9ca1bf60145f5
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 21:04:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,21 +5,3 @@ def key_for_min_value(name_hash)
 
 end
 
-def key_for_min_value(name_hash)
-
-  current_minimum_val = nil 
-  current_key_assoc_w_min_val = nil 
-  name_hash.each do |key, value|
-  
-    if current_minimum_val == nil 
-      current_minimum_val = value 
-      current_key_assoc_w_min_val = key 
-    else 
-      if current_minimum_val > value 
-        current_minimum_val = value
-        current_key_assoc_w_min_val = key 
-      end
-    end
-  end 
-  current_key_assoc_w_min_val 
-end 	end 
